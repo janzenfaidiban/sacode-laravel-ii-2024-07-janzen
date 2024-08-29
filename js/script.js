0231c551bfd51e5dd7c9713199cd940158458664
Janzen Faidiban <janzenfaidiban@gmail.com>
2024-08-29 19:58:51 +0900

    simpan hasil belajar

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// single line comment
+
+/*
+   multiple line comment
+   bisa tulis juga di sini
+*/
+
+
+var namaDepan = "Janzen";
+var namaBelakang = "Faidiban";
+
+document
+.getElementById("nama")
+.innerHTML = namaDepan + namaBelakang;
